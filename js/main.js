8f52be11dd85a9fa1e1273bb59b6bfb1a871b453
Sam <samtagert@gmail.com>
2017-09-17 00:48:52 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -28,15 +28,29 @@ var checkForMatch = function() {
 	} else {
 		alert("Sorry, try again");
 	}
-}
+};
 
-var flipCard = function(cardId) {
-	checkForMatch();
-	cardsInPlay.push(cards[cardId].rank);
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank +".");
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
-}
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+};
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
+
